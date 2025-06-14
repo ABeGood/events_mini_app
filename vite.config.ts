bf619ff521eca9de05721451bc06669833024171
ABeGood <openzzggl@gmail.com>
2025-06-14 14:37:40 +0200

    vite.config.ts base wo /

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/events-mini-app/',
+  base: '/events-mini-app',
   css: {
     preprocessorOptions: {
       scss: {
